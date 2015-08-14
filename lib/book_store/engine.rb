3455469a53ea653213630dc5126c83fc38c9e7c6
Chris Desch <cdesch@gmail.com>
2015-08-14 10:54:50 -0400

    SimpleForm Install
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- lib/book_store/engine.rb
+++ lib/book_store/engine.rb
@@ -1,5 +1,12 @@
+require 'bootstrap-sass'
+require 'friendly_id'
+require 'simple_form'
+
 module BookStore
   class Engine < ::Rails::Engine
+    require 'jquery-rails'
+    require 'jquery-ui-rails'
+    require 'ckeditor'
     isolate_namespace BookStore
   end
 end
